6995b3b0bd8c73dd1c5ef580ec5f65edbb06df15
sagar wankhede <swankhede639@gmail.com>
2024-08-07 14:56:58 +0530

    added page for pagination and print data on browser

--- src/component/PaginationReact.js
+++ src/component/PaginationReact.js
@@ -1,20 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 
 function PaginationReact() {
   const [products, setProducts] = useState([]);
-
-
+  const [page] = useState(1);
 
   const fetchProducts = async () => {
-    const res = await fetch(
-      "https://dummyjson.com/products?limit=100"
-    );
+    const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
     console.log(data);
-  }
-
-  if (data && data.products) {
-    setProducts(data.products);
+    
+    if (data && data.products) {
+      setProducts(data.products);
+    };
   }
 
   useEffect(() => {
@@ -22,7 +19,18 @@ function PaginationReact() {
   }, []);
   return (
     <div>
- 
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+            return (
+              <span key={prod.id} className="products__single">
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
